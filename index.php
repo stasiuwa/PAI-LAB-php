46959c1c2eef0198f03790d6dff8303dc4c38837
stasiuwa <pwinski013@gmail.com>
2023-10-10 18:52:02 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+    <head>
+        <meta charset="UTF-8">
+        <title>PAI LAB php</title>
+    </head>
+    <body>
+        <?php
+            echo"<h2>chuj</h2>";
+        ?>
+    </body>
+</html>
